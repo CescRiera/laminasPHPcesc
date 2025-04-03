fbfcca8682506f1ee37374032f99d426524d5ef2
CescRiera <ceriam@zend-ceriam.clotfje.net>
2025-04-03 19:27:35 +0200

    Commit 3

--- module/Application/src/Controller/AuthController.php
+++ module/Application/src/Controller/AuthController.php
@@ -1,4 +1,13 @@
 <?php
+    session_start();
+    session_unset();
+    $cookie_sessio = session_get_cookie_params();
+    setcookie("PHPSESSID","",time()-3600,$cookie_sessio['path'], $cookie_sessio['domain'], $cookie_sessio['secure'], $cookie_sessio['httponly']); //Neteja cookie de sessió
+    session_destroy();
+    header("Location: index.php");
+?>
+
+<?php
     require 'vendor/autoload.php';
 	use Laminas\Ldap\Ldap;
 
